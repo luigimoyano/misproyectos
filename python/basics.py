e4cdb2d16d0effa0dbd9d26e4efbe6e206e03bbf
Luis Moyano <luis.moyano@mo2o.com>
2013-04-01 16:55:33 +0200

    agregado ruby

--- python/basics.py
+++ python/basics.py
@@ -20,3 +20,13 @@ elif x == 1:
       print 'Single'
 else:
       print 'More'
+
+## Uso de funciones.
+def fib(n):
+     a, b = 0, 1
+     while a < n:
+         print a,
+         a, b = b, a+b
+
+# Now call the function we just defined:
+fib(2000) 
